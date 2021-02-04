425d73a6893388d4c453fe465a8542250aa736d4
rid14real <62150969+rid14real@users.noreply.github.com>
2021-02-04 08:09:02 +0100

    Calculating Annual Income

---
+++ Calculating Annual Income.py
@@ -0,0 +1,9 @@
+income = float(input("Enter the annual income: "))
+
+if income <= 85528:
+    tax = income*18/100 - 556.2
+else: 
+    tax = 14839.2 + ((income - 85528)*(32/100))
+
+tax = round(tax, 0)
+print("The tax is:", tax, "thalers")
